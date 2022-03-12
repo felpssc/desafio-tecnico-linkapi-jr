a13f93d098b0d358d30538b9318afa7e7ae2b243
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2022-03-11 23:15:48 -0300

    feat: add addresses listing

--- src/modules/users/useCases/ListUsersUseCase.ts
+++ src/modules/users/useCases/ListUsersUseCase.ts
@@ -1,12 +1,16 @@
 import { IUser } from "../entities/IUser";
 import { UsersRepository } from "../repositories/implementations/UsersRepository";
 import { IParams, IUsersRepository } from "../repositories/IUsersRepository";
+import { AddressesRepository } from "../repositories/implementations/AddressesRepository";
+import { IAddressesRepository } from "../repositories/IAddressesRepository";
 
 class ListUsersUseCase {
     private usersRepository: IUsersRepository;
+    private addressesRepository: IAddressesRepository;
 
     constructor() {
         this.usersRepository = new UsersRepository();
+        this.addressesRepository = new AddressesRepository();
     }
 
     async execute({
@@ -21,6 +25,13 @@ class ListUsersUseCase {
             sortBy,
             order
         });
+
+        for(const user of users) {
+            
+            const addresses = await this.addressesRepository.findByUserId(user.id);
+            user.addresses = addresses;
+
+        }
 
         return users;
     }
